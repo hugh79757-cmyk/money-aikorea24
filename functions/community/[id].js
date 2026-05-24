fe161410e30c1c91fa844ced02ba4945573cf44f
hugh79757-cmyk <hugh79757@gmail.com>
2026-05-24 21:23:30 +0700

    fix: 커뮤니티 글쓰기 페이지 Function 라우팅 충돌 수정

--- functions/community/[id].js
+++ functions/community/[id].js
@@ -1,4 +1,11 @@
 export async function onRequestGet({ params, env, request }) {
+  const id = params.id;
+  
+  // write, _ 는 정적 에셋으로 패스스루
+  if (id === 'write' || id === '_') {
+    return env.ASSETS.fetch(request);
+  }
+
   const url = new URL(request.url);
   const html = await env.ASSETS.fetch(
     new Request(url.origin + '/community/_/index.html')
